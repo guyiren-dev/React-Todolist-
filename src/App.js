ac4c952a4e773158e18919b5cab7ac81438af5fd
gyr <876156672@qq.com>
2020-06-02 20:37:48 +0800

    first_branch提交

--- src/App.js
+++ src/App.js
@@ -2,12 +2,51 @@ import React, { Component, Fragment } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      inputValue: "",
+      list: []
+    }
+  }
   render(){
       return (
         <Fragment>
-            
+          <div>
+            <input value = {this.state.inputValue}
+                   onChange = {this.handleValue.bind(this)}
+            />
+            <button onClick = {this.handleClickBtn.bind(this)}>提交</button>
+          </div>
+          <ul>
+             {this.state.list.map((item,index) => {
+               return (
+                 <li key = {index}
+                   onClick = {this.handleClickList.bind(this,index)} 
+               >{item}</li>
+               )
+             })} 
+          </ul>
         </Fragment>
       );
+  }
+  handleValue(e){
+    this.setState({
+      inputValue: e.target.value
+    })
+  }
+  handleClickBtn(){
+    this.setState({
+      list : [...this.state.list,this.state.inputValue],
+      inputValue: ''
+    })
+  }
+  handleClickList(index){
+    const list = [...this.state.list];
+    list.splice(index,1);
+    this.setState({
+      list: list
+    })
   }
 }
 
